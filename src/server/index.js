66ee433818db3f2265dbc1be9ee7a834f100cbea
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-10-22 21:59:46 +0200

    api servs first mock data

--- src/server/index.js
+++ src/server/index.js
@@ -10,6 +10,22 @@ app.get("/api/getUsername", (req, res) =>
 app.get("/system/liveliness", (req, res) => res.sendStatus(200) );
 app.get("/system/readiness", (req, res) => res.sendStatus(200) );
 
-app.get("/api", (req, res) => res.sendStatus(200) );
+// api mock
+const fs = require('fs')
+let file = __dirname + '/mock.json';
+let rawdata = fs.readFileSync(file);
+let jsonMock = JSON.parse(rawdata);
+app.get("/api", (req, res) =>
+  res.json(jsonMock)
+);
+app.get('/api/dns/:id', (req, res) => {
+   dnsMock = jsonMock["dns"]
+   dnsMock[0]["name"] = req.params.id
+   res.json(dnsMock)
+});
+app.get("/api/project/:id", (req, res) => {
+  projectMock = jsonMock["project"]
+  res.json(jsonMock)
+});
 
 app.listen(80, () => console.log("Listening on port 80!"));
