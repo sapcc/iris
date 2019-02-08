1139276774b67b42d4d1b9b51a2c82432754f7ec
andypf <andreas.pfau@sap.com>
2019-02-08 15:53:58 +0000

    upgrade react to 16.8

--- src/js/components/object/show.jsx
+++ src/js/components/object/show.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {
+  useState
+} from "react";
 import ReactJson from 'react-json-view'
 import {
   Link
@@ -7,126 +9,101 @@ import {
   FormGroup,
   Input,
   Col,
-  Label,
   UncontrolledTooltip,
   Card,
-  Button,
-  CardImg,
   CardTitle,
   CardHeader,
   CardText,
-  CardGroup,
-  CardSubtitle,
   CardBody,
   CardColumns
 } from 'reactstrap';
 
-export default class ObjectShow extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      relations: []
-    }
+export default ({
+  item,
+  dependencies
+}) => {
+  const [relations, setRelations] = useState([])
+
+  const addRelation = (name) => {
+    if (!name || name.length === 0) return
+    const newRelations = relations.slice()
+    newRelations.push(name)
+    setRelations(newRelations)
   }
 
-  addRelation = (name) => {
+  const removeRelation = (name) => {
     if (!name || name.length === 0) return
-    const relations = this.state.relations.slice()
-    relations.push(name)
-    this.setState({
-      relations
-    })
-  }
-
-  removeRelation = (name) => {
-    if (!name || name.length === 0) return
-    const index = this.state.relations.indexOf(name)
+    const index = relations.indexOf(name)
     if (index < 0) return
-    let relations = this.state.relations.slice()
-    relations.splice(index, 1)
-    this.setState({
-      relations
-    })
+    let newRelations = relations.slice()
+    newRelations.splice(index, 1)
+    setRelations(newRelations)
   }
 
-  relationItems = (name) => {
-    const {
-      dependencies
-    } = this.props
-
-    const relations = dependencies.isFetching ? [] : dependencies.items[name]
-    return relations || []
+  const relationItems = (name) => {
+    const items = dependencies.isFetching ? [] : dependencies.items[name]
+    return items || []
   }
 
-  filterRelations = () => {
-    if (this.props.dependencies.isFetching || !this.props.dependencies.items) return
+  const filterRelations = () => {
+    if (dependencies.isFetching || !dependencies.items) return
 
-    return Object.keys(this.props.dependencies.items).filter(a =>
-      !this.state.relations.find(u => a == u)
+    return Object.keys(dependencies.items).filter(a =>
+      !relations.find(u => a == u)
     )
   }
 
-  render() {
-    const {
-      item,
-      dependencies
-    } = this.props
 
-    return (
-      <div className="container-fluid">
-        
-        <Card className="text-center">
-          <CardBody>
-            <CardTitle>{item.object_type}</CardTitle>
-            <CardText>{item.name}</CardText>
-            
-            {!dependencies || dependencies.isFetching 
-                ? <span className="spinner"/>
-                :
-                <React.Fragment>
-                  <FormGroup row className="justify-content-md-center">
-                    <Col sm={3}>
-                    <Input type="select" onChange={(e) => this.addRelation(e.target.value)} value=''>
-                      <option value=''>Add Relation</option>
-                      {this.filterRelations().map((type,index) => 
-                        <option key={index} value={type}>{type}</option>
-                      )}
-                    </Input>  
-                  </Col>
-                  </FormGroup>
-                </React.Fragment>
-            }
-          </CardBody>
-        </Card>
-        <br />
-
-        <CardColumns>
-          {this.state.relations.map(type =>
-            <Card key={type}>
-              <CardHeader>
-                {type}
-              <span className="float-right clickable close-icon" onClick={(e) => this.removeRelation(type)}>
-                <i className="fa fa-times text-info"></i>
-              </span>
-            </CardHeader>
-            <CardBody>
-              <ul>
-                {this.relationItems(type).map((obj,index) => 
-                  <li key={index}>
-                    <span id={`tooltip-${type}-${index}`}>{obj.name}</span>
-                      <br/>
-                      <span className="small text-info">{obj._path}</span>
-                    </li>  
-                  )}
-                </ul>
-              </CardBody>
-            </Card>
-          )}
-        </CardColumns>
-                  
+  return (
+    <div className="container-fluid">
+      <Card className="text-center">
+        <CardBody>
+          <CardTitle>{item.object_type}</CardTitle>
+          <CardText>{item.name}</CardText>
           
+          {!dependencies || dependencies.isFetching 
+              ? <span className="spinner"/>
+              :
+              <React.Fragment>
+                <FormGroup row className="justify-content-md-center">
+                  <Col sm={3}>
+                  <Input type="select" onChange={(e) => addRelation(e.target.value)} value=''>
+                    <option value=''>Add Relation</option>
+                    {filterRelations().map((type,index) => 
+                      <option key={index} value={type}>{type}</option>
+                    )}
+                  </Input>  
+                </Col>
+                </FormGroup>
+              </React.Fragment>
+          }
+        </CardBody>
+      </Card>
+      <br />
 
-      </div>
-    )
-  }
+      <CardColumns>
+        {relations.map(type =>
+          <Card key={type}>
+            <CardHeader>
+              {type}
+            <span className="float-right clickable close-icon" onClick={(e) => removeRelation(type)}>
+              <i className="fa fa-times text-info"></i>
+            </span>
+          </CardHeader>
+          <CardBody>
+            <ul>
+              {relationItems(type).map((obj,index) => 
+                <li key={index}>
+                  <span id={`tooltip-${type}-${index}`}>{obj.name}</span>
+                    <br/>
+                    <span className="small text-info">{obj._path}</span>
+                  </li>  
+                )}
+              </ul>
+            </CardBody>
+          </Card>
+        )}
+      </CardColumns>
+    </div>
+  )
 }
